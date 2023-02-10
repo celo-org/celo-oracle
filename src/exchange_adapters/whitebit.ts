40598281b2eddcd3cf1076e6c79d4e1c5e006013
Bayological <6872903+bayological@users.noreply.github.com>
2023-02-10 18:56:22 +0100

    feat: add whitebit exchange adapter (#150)

---
+++ src/exchange_adapters/whitebit.ts
@@ -0,0 +1,113 @@
+import { BaseExchangeAdapter, ExchangeDataType, Ticker, Trade } from './base'
+
+import { Exchange } from '../utils'
+
+export class WhitebitAdapter extends BaseExchangeAdapter {
+  baseApiUrl = 'https://whitebit.com/api/v4/public/'
+  readonly _exchangeName = Exchange.WHITEBIT
+  readonly _certFingerprint256 = ''
+
+  private static readonly tokenSymbolMap = this.standardTokenSymbolMap
+
+  protected generatePairSymbol(): string {
+    const base = WhitebitAdapter.tokenSymbolMap.get(this.config.baseCurrency)?.toUpperCase()
+    const quote = WhitebitAdapter.tokenSymbolMap.get(this.config.quoteCurrency)?.toUpperCase()
+
+    return `${base}_${quote}`
+  }
+
+  async fetchTrades(): Promise<Trade[]> {
+    // "An empty array in TypeScript may seem insignificant,
+    //    but it holds within it the infinite potential of
+    //          all the elements yet to come."
+    // -- @bayological --
+
+    return []
+  }
+
+  async fetchTicker(): Promise<Ticker> {
+    const json = await this.fetchFromApi(ExchangeDataType.TICKER, `ticker`)
+    const tickerData = json[this.generatePairSymbol()]
+
+    if (!tickerData) {
+      throw new Error(`Ticker data not found for ${this.generatePairSymbol()}`)
+    }
+
+    // Get orderbook data as ticker data does not contain bid/ask
+    const orderBookData = await this.fetchFromApi(
+      ExchangeDataType.TICKER,
+      `orderbook/${this.generatePairSymbol()}?limit=1&level=2&`
+    )
+
+    return this.parseTicker({
+      ...tickerData,
+      ask: orderBookData.asks[0][0],
+      bid: orderBookData.bids[0][0],
+    })
+  }
+
+  /**
+   *
+   * @param json parsed response from whitebits's ticker endpoint
+   * https://whitebit.com/api/v4/public/ticker
+   *
+   *   "1INCH_UAH": {
+   *     "base_id": 8104,
+   *     "quote_id": 0,
+   *     "last_price": "20.991523",
+   *     "quote_volume": "1057381.44765064",
+   *     "base_volume": "48537.28",
+   *     "isFrozen": false,
+   *     "change": "-4.71"
+   *   },
+   */
+  parseTicker(tickerData: any): Ticker {
+    const ticker = {
+      ...this.priceObjectMetadata,
+      ask: this.safeBigNumberParse(tickerData.ask)!,
+      baseVolume: this.safeBigNumberParse(tickerData.base_volume)!,
+      bid: this.safeBigNumberParse(tickerData.bid)!,
+      lastPrice: this.safeBigNumberParse(tickerData.last_price)!,
+      quoteVolume: this.safeBigNumberParse(tickerData.quote_volume)!,
+      timestamp: 0, // Timestamp is not provided by Whitebit and is not used by the oracle
+    }
+    this.verifyTicker(ticker)
+    return ticker
+  }
+
+  /**
+   * Checks status of orderbook
+   * [GET] /api/v4/public/markets
+   * [
+   *  {
+   *    "name": "SON_USD",         // Market pair name
+   *    "stock": "SON",            // Ticker of stock currency
+   *    "money": "USD",            // Ticker of money currency
+   *    "stockPrec": "3",          // Stock currency precision
+   *    "moneyPrec": "2",          // Precision of money currency
+   *    "feePrec": "4",            // Fee precision
+   *    "makerFee": "0.001",       // Default maker fee ratio
+   *    "takerFee": "0.001",       // Default taker fee ratio
+   *    "minAmount": "0.001",      // Minimal amount of stock to trade
+   *    "minTotal": "0.001",       // Minimal amount of money to trade
+   *    "tradesEnabled": true,     // Is trading enabled
+   *    "isCollateral": true,      // Is margin trading enabled
+   *    "type": "spot"             // Market type. Possible values: "spot", "futures"
+   *  }
+   *
+   * @returns bool
+   */
+  async isOrderbookLive(): Promise<boolean> {
+    const marketInfoData = await this.fetchFromApi(ExchangeDataType.ORDERBOOK_STATUS, `markets`)
+
+    const filteredMarketInfo = marketInfoData.filter(
+      (market: any) => market.name === this.generatePairSymbol()
+    )
+
+    if (filteredMarketInfo.length !== 1) {
+      throw new Error(`Market info not found for ${this.generatePairSymbol()}`)
+    }
+
+    return filteredMarketInfo[0].tradesEnabled && filteredMarketInfo[0].type === 'spot'
+  }
+}
---
+++ test/exchange_adapters/whitebit.test.ts
@@ -0,0 +1,163 @@
+import { Exchange, ExternalCurrency } from '../../src/utils'
+import { ExchangeAdapterConfig, ExchangeDataType } from '../../src/exchange_adapters/base'
+
+import BigNumber from 'bignumber.js'
+import { WhitebitAdapter } from '../../src/exchange_adapters/whitebit'
+import { baseLogger } from '../../src/default_config'
+
+// Mock data
+const validMockTickerJson = {
+  ask: 0.95,
+  base_volume: 23401032.8528,
+  bid: 0.9,
+  last_price: 0.9998,
+  quote_volume: 23399576.58906071,
+  timestamp: 0,
+}
+
+const inValidMockTickerJson = {
+  quote_volume: 23399576.58906071,
+  timestamp: 0,
+}
+
+const mockValidTickerData = {
+  '1INCH_BTC': {
+    base_id: 8104,
+    quote_id: 1,
+    last_price: '0.0000246',
+    quote_volume: '1.16888304',
+    base_volume: '48268',
+    isFrozen: false,
+    change: '0.94',
+  },
+  USDC_USDT: {
+    base_id: 8104,
+    quote_id: 1,
+    last_price: '0.9876',
+    quote_volume: '1.16888304',
+    base_volume: '48268',
+    isFrozen: false,
+    change: '0.94',
+  },
+}
+
+const mockValidOrderbookData = {
+  timestamp: 1676047317,
+  asks: [['1.001', '5486968.7515']],
+  bids: [['0.999', '385192']],
+}
+
+describe('Whitebit adapter', () => {
+  let whitebitAdapter: WhitebitAdapter
+
+  const config: ExchangeAdapterConfig = {
+    baseCurrency: ExternalCurrency.USDC,
+    baseLogger,
+    quoteCurrency: ExternalCurrency.USDT,
+  }
+  beforeEach(() => {
+    whitebitAdapter = new WhitebitAdapter(config)
+  })
+
+  afterEach(() => {
+    jest.clearAllTimers()
+    jest.clearAllMocks()
+  })
+
+  describe('fetchFromApi', () => {
+    let fetchFromApiSpy: jest.SpyInstance
+    beforeEach(() => {
+      fetchFromApiSpy = jest.spyOn(whitebitAdapter, 'fetchFromApi')
+      fetchFromApiSpy
+        .mockReturnValueOnce(mockValidTickerData)
+        .mockReturnValueOnce(mockValidOrderbookData)
+    })
+
+    it('calls correct endpoints on whitebit api', async () => {
+      fetchFromApiSpy
+        .mockReturnValueOnce(mockValidTickerData)
+        .mockReturnValueOnce(mockValidOrderbookData)
+
+      await whitebitAdapter.fetchTicker()
+      expect(fetchFromApiSpy).toHaveBeenCalledTimes(2)
+      expect(fetchFromApiSpy).toHaveBeenNthCalledWith(1, ExchangeDataType.TICKER, 'ticker')
+      expect(fetchFromApiSpy).toHaveBeenNthCalledWith(
+        2,
+        ExchangeDataType.TICKER,
+        'orderbook/USDC_USDT?limit=1&level=2&'
+      )
+    })
+
+    it('calls parseTicker with the right parameters', async () => {
+      const parseTickerSpy = jest.spyOn(whitebitAdapter, 'parseTicker')
+      await whitebitAdapter.fetchTicker()
+
+      expect(parseTickerSpy).toHaveBeenCalledTimes(1)
+      expect(parseTickerSpy).toHaveBeenCalledWith({
+        ...mockValidTickerData.USDC_USDT,
+        ask: mockValidOrderbookData.asks[0][0],
+        bid: mockValidOrderbookData.bids[0][0],
+      })
+    })
+  })
+
+  describe('parseTicker', () => {
+    it('handles a response that matches the documentation', async () => {
+      const ticker = await whitebitAdapter.parseTicker(validMockTickerJson)
+
+      expect(ticker).toEqual({
+        source: Exchange.WHITEBIT,
+        symbol: whitebitAdapter.standardPairSymbol,
+        ask: new BigNumber(0.95),
+        baseVolume: new BigNumber(23401032.8528),
+        bid: new BigNumber(0.9),
+        lastPrice: new BigNumber(0.9998),
+        quoteVolume: new BigNumber('23399576.58906071'),
+        timestamp: 0,
+      })
+    })
+
+    it('throws an error when a json field mapped to a required ticker field is missing', () => {
+      expect(() => {
+        whitebitAdapter.parseTicker(inValidMockTickerJson)
+      }).toThrowError(new Error('bid, ask, lastPrice, baseVolume not defined'))
+    })
+  })
+
+  describe('isOrderbookLive', () => {
+    const statusResponse = {
+      name: 'USDC_USDT',
+      tradesEnabled: true,
+      type: 'spot',
+    }
+
+    it('returns false when trading is not enabled', async () => {
+      jest.spyOn(whitebitAdapter, 'fetchFromApi').mockReturnValue(
+        Promise.resolve([
+          {
+            ...statusResponse,
+            tradesEnabled: false,
+          },
+        ])
+      )
+      expect(await whitebitAdapter.isOrderbookLive()).toEqual(false)
+    })
+
+    it('returns false when market is not spot', async () => {
+      jest.spyOn(whitebitAdapter, 'fetchFromApi').mockReturnValue(
+        Promise.resolve([
+          {
+            ...statusResponse,
+            type: 'spotty',
+          },
+        ])
+      )
+      expect(await whitebitAdapter.isOrderbookLive()).toEqual(false)
+    })
+
+    it('returns true when trades are enabled and market is spot', async () => {
+      jest.spyOn(whitebitAdapter, 'fetchFromApi').mockReturnValue(Promise.resolve([statusResponse]))
+      expect(await whitebitAdapter.isOrderbookLive()).toEqual(true)
+    })
+  })
+})
