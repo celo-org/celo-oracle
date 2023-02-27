fd59708b0801d91ba463305582403d3485b16585
tobikuhlmann <tobias.kuhlmann@mentolabs.xyz>
2023-02-27 11:48:26 +0100

    chore(gemini adapter): correct comments

--- src/exchange_adapters/gemini.ts
+++ src/exchange_adapters/gemini.ts
@@ -5,7 +5,7 @@ export class GeminiAdapter extends BaseExchangeAdapter {
   baseApiUrl = 'https://api.gemini.com/v1/'
 
   readonly _exchangeName = Exchange.GEMINI
-  // Amazon cert
+  // Amazon bitget.com cert
   readonly _certFingerprint256 =
     'F5:5F:9F:FC:B8:3C:73:45:32:61:60:1C:7E:04:4D:B1:5A:0F:03:4B:93:C0:58:30:F2:86:35:EF:88:9C:F6:70'
 
@@ -44,9 +44,9 @@ export class GeminiAdapter extends BaseExchangeAdapter {
    *     "bid": "977.35",                  // The highest bid currently available
    *     "last": "977.65",                 // The price of the last executed trade
    *     "volume": {                       // Information about the 24 hour volume on the exchange. See below
-   *       "BTC": "2210.505328803",        // The end of the 24-hour period over which volume was measured
-   *       "USD": "2135477.463379586263",  // (price symbol, e.g. "USD") The volume denominated in the price currency
-   *       "timestamp": 1483018200000      // (price symbol, e.g. "USD") The volume denominated in the quantity currency
+   *       "BTC": "2210.505328803",        // (price symbol, e.g. "USD") The volume denominated in the price currency
+   *       "USD": "2135477.463379586263",  // (price symbol, e.g. "USD") The volume denominated in the quantity currency
+   *       "timestamp": 1483018200000      // The end of the 24-hour period over which volume was measured
    *     }
    *   }
    */
@@ -66,7 +66,7 @@ export class GeminiAdapter extends BaseExchangeAdapter {
     return ticker
   }
 
-  /**
+/**
    * Checks if the orderbook for the relevant pair is live. If it's not, the price
    * data from Ticker + Trade endpoints may be inaccurate.
    *
@@ -84,12 +84,12 @@ export class GeminiAdapter extends BaseExchangeAdapter {
    *
    * @returns bool
    */
-  async isOrderbookLive(): Promise<boolean> {
-    const res = await this.fetchFromApi(
-      ExchangeDataType.ORDERBOOK_STATUS,
-      `symbols/details/${this.pairSymbol}`
-    )
+async isOrderbookLive(): Promise<boolean> {
+  const res = await this.fetchFromApi(
+    ExchangeDataType.ORDERBOOK_STATUS,
+    `symbols/details/${this.pairSymbol}`
+  )
 
-    return res.status === 'open'
-  }
+  return res.status === 'open'
+}
 }
