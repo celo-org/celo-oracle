ea7bc43132bcfedf4267eae6df0eb7e3074b01a8
Nina Barbakadze <barbakadzeninaa@gmail.com>
2023-09-07 14:02:35 +0200

    style: lint

--- src/exchange_adapters/binance_us.ts
+++ src/exchange_adapters/binance_us.ts
@@ -6,6 +6,6 @@ export class BinanceUSAdapter extends BinanceAdapter implements ExchangeAdapter 
   baseApiUrl = 'https://api.binance.us/api/v3'
   readonly _exchangeName = Exchange.BINANCEUS
   // *.binance.us - validity not after: 11/09/2024, 01:59:59 CEST
-   _certFingerprint256 =
+  _certFingerprint256 =
     '45:48:18:31:2F:B1:60:5F:70:EA:FA:B8:67:B1:A5:5A:05:96:BE:74:66:C7:60:E4:F7:AF:D3:3F:0A:2E:D9:32'
 }
