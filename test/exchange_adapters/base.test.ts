3170eca50fe63722f090aa98c83b4e410cde8a17
Paul Lange <palango@gmx.de>
2024-03-04 17:34:37 +0100

    fix style

--- test/exchange_adapters/base.test.ts
+++ test/exchange_adapters/base.test.ts
@@ -133,7 +133,7 @@ describe('BaseExchangeAdapter', () => {
         await expect(async () =>
           adapter.fetchFromApi(ExchangeDataType.TICKER, mockTickerEndpoint)
         ).rejects.toThrowError(
-          'Failed to parse JSON response: FetchError: invalid json response body at  reason: Unexpected token \'<\', \"<html>blah\"... is not valid JSON'
+          'Failed to parse JSON response: FetchError: invalid json response body at  reason: Unexpected token \'<\', "<html>blah"... is not valid JSON'
         )
         expect(metricCollector.exchangeApiRequestError).toBeCalledWith(
           ...metricArgs,
