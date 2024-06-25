5828022fbfaa44ca299d1f72ceb0f0a230943e98
philbow61 <philip.raetsch@gmail.com>
2024-06-25 11:42:48 +0200

    fix: EUROC to EURC renaming on Coinbase

--- test/exchange_adapters/base.test.ts
+++ test/exchange_adapters/base.test.ts
@@ -133,7 +133,7 @@ describe('BaseExchangeAdapter', () => {
         await expect(async () =>
           adapter.fetchFromApi(ExchangeDataType.TICKER, mockTickerEndpoint)
         ).rejects.toThrowError(
-          'Failed to parse JSON response: FetchError: invalid json response body at  reason: Unexpected token \'<\', "<html>blah"... is not valid JSON'
+          'Failed to parse JSON response: FetchError: invalid json response body at  reason: Unexpected token < in JSON at position 0'
         )
         expect(metricCollector.exchangeApiRequestError).toBeCalledWith(
           ...metricArgs,
